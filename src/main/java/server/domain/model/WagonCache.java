5ed07314ab2755f763e11fd23817be0ea51f6f20
Ara1228 <smqpro@gmail.com>
2019-06-17 16:54:52 +0600

    Domain models added

--- src/main/java/server/domain/model/WagonCache.java
+++ src/main/java/server/domain/model/WagonCache.java
@@ -8,18 +8,36 @@ public class WagonCache {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
-    @OneToOne(cascade = CascadeType.ALL, fetch =  FetchType.EAGER, mappedBy = "wagonCacheId", optional = false)
-    @JoinColumn(name = "vagon_id")
-    private VagonInfo vagon_id;
-
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "wagonCacheId")
     private List<Position> positions;
-
-    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "wagonCacheId",optional = false)
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "wagonCacheId", optional = false)
     @JoinColumn(name = "user_wagon_id")
     private UserWagon userWagonId;
 
     public WagonCache() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public List<Position> getPositions() {
+        return positions;
+    }
+
+    public void setPositions(List<Position> positions) {
+        this.positions = positions;
+    }
+
+    public UserWagon getUserWagonId() {
+        return userWagonId;
+    }
+
+    public void setUserWagonId(UserWagon userWagonId) {
+        this.userWagonId = userWagonId;
     }
 }
